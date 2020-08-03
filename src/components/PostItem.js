5f7ed4d4fd1d7b762c2a69affa9afc33fc50b940
Wellington N <wellingto_07@hotmail.com>
2020-08-03 16:47:29 -0300

    postComments

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -11,3 +11,20 @@ function PostHeader({ author, date }) {
     </div>
   );
 }
+
+function PostComments({ comments }) {
+  return (
+    <div className="post-comments">
+      <div className="divider" />
+      {comments.map((comment) => (
+        <div key={comment.id} className="comment">
+          <img className="avatar" src={comment.author.avatar} />
+          <p>
+            <span>{comment.author.name}</span>
+            {comment.content}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
