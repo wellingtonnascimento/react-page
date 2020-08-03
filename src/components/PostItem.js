c407e85f2bb397f2d5d3c5785360fa77b37f05c6
Wellington N <wellingto_07@hotmail.com>
2020-08-03 16:44:49 -0300

    postheader

---
+++ src/components/PostItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PostHeader({ author, date }) {
+  return (
+    <div className="post-header">
+      <img className="avatar" src={author.avatar} />
+      <div className="details">
+        <span>{author.name}</span>
+        <span>{date}</span>
+      </div>
+    </div>
+  );
+}
