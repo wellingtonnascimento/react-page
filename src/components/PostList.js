851de8faf59f328a2613ca468caff8f20f27ff1c
Wellington N <wellingto_07@hotmail.com>
2020-08-04 08:41:19 -0300

    logo header

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -12,17 +12,17 @@ class PostList extends Component {
           avatar: "https://i.pravatar.cc/150?img=1",
         },
         date: "04 Jun 2019",
-        content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
+        content: "Pessoal, alguém sabe está contratando dev?",
         comments: [
           {
             id: 2,
             author: {
-              name: "Diego Fernandes",
+              name: "Jose da Silva",
               avatar: "https://avatars2.githubusercontent.com/u/2254731?v=4",
             },
             date: "04 Jun 2019",
             content:
-              "A Rocketseat está sempre em busca de novos membros para o time, e geralmente ficamos de olho em quem se destaca no Bootcamp, inclusive 80% do nosso time de devs é composto por alunos do Bootcamp. Além disso, se você tem vontade de ensinar gravando vídeos e criando posts, pode me chamar no Discord! (Sério, me chamem mesmo, esse comentário é real)",
+              "A Linkedin está sempre mostrando novas vagas para varias empresas",
           },
         ],
       },
@@ -54,7 +54,7 @@ class PostList extends Component {
             },
             date: "04 Jun 2019",
             content:
-              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma pra ver qual é desse Bootcamp GoStack, dizem que os devs saem de lá com super poderes",
+              "Que maaaaaassa! Estou pensando em me inscrever na próxima turma, dizem que os devs saem de lá com super poderes",
           },
         ],
       },
@@ -66,7 +66,7 @@ class PostList extends Component {
         },
         date: "04 Jun 2019",
         content:
-          "Fala galera, beleza?\nEstou fazendo o Bootcamp GoStack e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
+          "Fala galera, beleza?\nEstou fazendo o Bootcamp e está sendo muito massa! Alguém mais aí fazendo? Comenta aí na publicação para trocarmos uma idéia",
         comments: [
           {
             id: 4,
